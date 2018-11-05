d66353286db659b1b39dc68d1e067955391e9336
Jan Nowicki <pxlmoose@gmail.com>
2018-11-05 20:45:38 +0100

    set up webpack with babel, dev-server and dev or production modes

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// import './utils.js' Remainder- with weback imports you can skip file's extention like .js
+import { add } from './utils.js';
+
+console.log('index.js');
+console.log(add(40, 30));
+
---
+++ src/utils.js
@@ -0,0 +1,3 @@
+console.log('utils.js');
+
+export const add = (a, b) => a + b;
